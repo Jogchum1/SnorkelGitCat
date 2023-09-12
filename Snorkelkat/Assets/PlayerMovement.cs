13e790aa28b1e983a7cae32a502858eb44829cf3
Jogchum1 <jogchum1@hotmail.nl>
2023-09-12 11:14:16 +0200

    Update PlayerMovement.cs

--- Snorkelkat/Assets/PlayerMovement.cs
+++ Snorkelkat/Assets/PlayerMovement.cs
@@ -37,6 +37,7 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
         {
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+            jumpBufferCount = 0;
         }
 
         //Hantime
