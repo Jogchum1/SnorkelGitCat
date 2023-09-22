1070e208f6ded0567937a83360b677d5acbbbd21
Jogchum1 <jogchum1@hotmail.nl>
2023-09-22 11:17:33 +0200

    Started setup

---
+++ Snorkelkat/Assets/Scripts/Room.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+    public List<SpawnPoint> spawnPoints = new List<SpawnPoint>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
